b1d63850be30c0d43336509f311db52ddc38e7d0
Wendel Evangelista <107012134+wendelevs@users.noreply.github.com>
2024-05-01 19:52:03 -0300

    development of the content and style of project

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./**/*.{html, js}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      'sans': ['Poppins', 'sans-serif']
+    },
+    extend: {
+      backgroundImage: {
+        "home": "url('/assets/bg.png')"
+      }
+    },
   },
   plugins: [],
 }
