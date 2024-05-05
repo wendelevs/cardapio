fc4628b022bcd3a77bcecd0099579da11f3287bd
Wendel Evangelista <107012134+wendelevs@users.noreply.github.com>
2024-05-05 15:37:45 -0300

    ending the project

--- script.js
+++ script.js
@@ -137,7 +137,18 @@ checkoutBtn.addEventListener("click", function() {
     const isOpen = checkRestaurantOpen()
 
     if (!isOpen) {
-        alert("RESTAURANTE FECHADO NO MOMENTO!")
+        Toastify({
+            text: "Ops, o restaurante está fechado!",
+            duration: 3000,
+            close: true,
+            gravity: "top", // `top` or `bottom`
+            position: "center", // `left`, `center` or `right`
+            stopOnFocus: true, // Prevents dismissing of toast on hover
+            style: {
+                background: "#ef4444",
+            },
+        }).showToast()
+
         return
     }
     
